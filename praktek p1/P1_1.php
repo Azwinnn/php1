1da024dbec2caff1e587d1d05a0079f2eb961ca1
Azwinnn <127075046+Azwinnn@users.noreply.github.com>
2023-04-16 23:40:18 +0700

    Add files via upload

---
+++ praktek p1/P1_1.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+    <title> Variable</title>
+</head>
+
+<body>
+    <?php
+    $nilai_1 = 10;
+    $nilai_2 = 3;
+    $nilai_3 = 2 * $nilai_1 + 8 * $nilai_2;
+    echo "nilai = ", $nilai_3;
+    echo "<br>";
+    $jumlah = $nilai_1 + $nilai_2;
+    echo "hasil dari $nilai_1 + $nilai_1 adalah : $jumlah";
+    echo "<br><br>";
+    echo "\"Nama : Ahmad Azwin Asyrafi\" <br>";
+    echo "NPM : 21082010168";
+    ?>
+</body>
+
+</html>
---
+++ praktek p1/P1_2.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+    <title> Pemrograman PHP dengan Array</title>
+</head>
+
+<body>
+    <?php
+    //penulisan array dapat dibuat seperti berikut
+    $nama[] = "Ahmad ";
+    $nama[] = "Azwin ";
+    $nama[] = "Asyrafi ";
+    echo $nama[0] . $nama[1] . $nama[2] ;
+    echo "<br>";
+    ?>
+</body>
+
+</html>
